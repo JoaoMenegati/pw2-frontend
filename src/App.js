a48c22e1ba7223377d45db58c2b1d65d9ad7a743
= <vbelloli@outlook.com>
2021-05-15 19:58:41 -0300

    Finalizado quiz em react

--- src/App.js
+++ src/App.js
@@ -1,40 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { Questionario } from './components';
 
-const API_URL = 'https://opentdb.com/api.php?amount=10&category=14&difficulty=easy&type=multiple';
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple';
 
 function App() {
   const [questions, setQuestions] = useState([]);
-  const [currentQuestion, setCurrentQuestions] = useState(undefined);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showAnswers, setShowAnswers] = useState(false);
 
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
-        setQuestions(data.results);
-        setCurrentQuestions(data.results[0]);
+        const questions = data.results.map((question) => ({
+          ...question,
+          answers: [
+            question.correct_answer,
+            ...question.incorrect_answers
+          ].sort(() => Math.random() - 0.5)
+        }))
+
+        setQuestions(questions);
       })
   }, []);
 
-  const handleAnswer = (answer) => {
-    //verificar a questão
-    
-    //mostrar próxima questão
+  const handleAnswer = (answer) => {  
+    if(!showAnswers){
+      if(answer === questions[currentIndex].correct_answer){
+        //Aumenta a pontuação
+        setScore(score + 1);
+      }
+    }
 
-    //aumentar pontuação se acertar
+    setShowAnswers(true);
   };
 
-  return (questions.length > 0 ? (
-    <div className="container">
-      {currentQuestion && (
-        <Questionario data={currentQuestion}
-        handleAnswer={handleAnswer} />
-      )}
-    </div>
-  ) : (
-    <h2 className="text-2xl text-white font-bold">Carregando Questões...</h2>
-  )
-  );
+  const handleNextQuestion = () => {
+    setCurrentIndex(currentIndex + 1);
+    setShowAnswers(false);
+  }
+
+  return questions.length > 0 ? (
+      <div className="container">
+        {currentIndex >= questions.length ? (
+          <h1 className="text-3xl text-white font-bold">Sua pontuação e: {score}.</h1>
+        ) : (
+          <Questionario data={questions[currentIndex]}
+          showAnswers={showAnswers}
+          handleNextQuestion={handleNextQuestion}
+          handleAnswer={handleAnswer} />
+        )}
+      </div>
+    ) : (
+      <h2 className="text-2xl text-white font-bold">Carregando Questões...</h2>
+    );
 }
 
 export default App;
